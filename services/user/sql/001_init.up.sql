52948153da59f0dfd8d5726eadfb9f15177ea3f2
Aaron <10927621+abennett@users.noreply.github.com>
2021-08-02 14:08:52 -0400

    add unique constraint on usernames

--- services/user/sql/001_init.up.sql
+++ services/user/sql/001_init.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-    name TEXT NOT NULL,
+    name TEXT UNIQUE NOT NULL,
     password bytea NOT NULL,
     tags text[] DEFAULT '{}' NOT NULL
 );
